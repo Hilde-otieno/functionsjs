9746ea18eddea0feaecbdccd6011e763bbd21a2d
Hilde-Otieno <meghanadhiambootieno@gmail.com>
2024-03-30 00:57:45 +0300

    made changes

--- functions.js
+++ functions.js
@@ -1,8 +1,11 @@
 function names(array){
     const names = ["Megan", "Agnes", "Shalom", "Mwanasha"]
 
-    names.sort();
-    names.reverse();
     console.log(names.reverse().sort());
     return {return:names};
+}
+let numbers=[23,54,38,98,10];
+let newNumbers=[];
+numbers.forEach(function(newNumbers)){
+    newNumbers.push(newNumbers*2)
 }
